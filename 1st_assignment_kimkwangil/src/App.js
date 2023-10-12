8c35a036676ec9cad58115558b7c855215c481ae
pangil5634 <oksk6681@naver.com>
2023-10-13 03:16:33 +0900

    231013 test commit

--- 1st_assignment_kimkwangil/src/App.js
+++ 1st_assignment_kimkwangil/src/App.js
@@ -1,9 +1,10 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import MyPage from './MyPage';
 
 function App() {
     return (
-        <MyPage></MyPage>  
+        <Route></Route>
     );
 
 }
